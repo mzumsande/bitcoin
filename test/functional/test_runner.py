d1d993cda6bc9a42e0be9582afbf7ec1b60a4103
Martin Zumsande <mzumsande@gmail.com>
2022-05-05 16:30:19 +0200

    test: Add interface_uprobe_signal test
    This test goes through the init and shutdown process, sending the node
    termination signals and makes sure that no corruption happens

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -192,6 +192,7 @@ BASE_SCRIPTS = [
     'wallet_reorgsrestore.py',
     'interface_http.py',
     'interface_rpc.py',
+    'interface_uprobe_signals.py',
     'interface_usdt_coinselection.py',
     'interface_usdt_mempool.py',
     'interface_usdt_net.py',
