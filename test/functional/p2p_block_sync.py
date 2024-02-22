1819fe7e0b0fb30a3bfa709501dca50facdb67b1
Martin Zumsande <mzumsande@gmail.com>
2024-02-22 16:42:06 -0500

    demonstrate sync failure

--- test/functional/p2p_block_sync.py
+++ test/functional/p2p_block_sync.py
@@ -2,11 +2,6 @@
 # Copyright (c) 2022 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Test block download
-
-Ensure that even in IBD, we'll eventually sync chain from inbound peers
-(whether we have only inbound peers or both inbound and outbound peers).
-"""
 
 from test_framework.test_framework import BitcoinTestFramework
 
@@ -18,20 +13,16 @@ class BlockSyncTest(BitcoinTestFramework):
 
     def setup_network(self):
         self.setup_nodes()
-        # Construct a network:
-        # node0 -> node1 -> node2
-        # So node1 has both an inbound and outbound peer.
-        # In our test, we will mine a block on node0, and ensure that it makes
-        # to to both node1 and node2.
-        self.connect_nodes(0, 1)
-        self.connect_nodes(1, 2)
 
     def run_test(self):
-        self.log.info("Setup network: node0->node1->node2")
-        self.log.info("Mining one block on node0 and verify all nodes sync")
-        self.generate(self.nodes[0], 1)
-        self.log.info("Success!")
-
+        self.connect_nodes(0, 1) #Test succeeds if I comment this line out!
+        # Node 2 (not connected to anyone) generates some blocks
+        self.generate(self.nodes[2], 10, sync_fun=self.no_op)
+        # connect node 1 and 2 node 1 should request blocks from node2
+        self.connect_nodes(1, 2)
+        self.sync_blocks(nodes=self.nodes[1:3], timeout=5)
+        # but node 1 doesn't send a getheaders msg and doesn't sync because it thinks it's still syncing with node 0 (fSyncStarted=True)
+        # even though it could have inferred from the non-full headers msg from node0 that there is nothing more to sync!
 
 if __name__ == '__main__':
     BlockSyncTest().main()
